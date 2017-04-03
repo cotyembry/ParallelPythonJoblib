5d8daf8ad0aa055201af55b4aa4a151def359619
Coty Embry <Coty Embry>
2017-04-03 00:27:05 -0500

    commiting stable version

--- parallelInPythonJoblib.py
+++ parallelInPythonJoblib.py
@@ -13,10 +13,16 @@ import math
 
 beginningTime = time.monotonic()
 
-def square(numberToSquare, numberOfFunctions):
-	# print('in square, time = ' + str(time.monotonic()))
-	numberPlusScalarSquared = (numberToSquare + numberOfFunctions) * (numberToSquare + numberOfFunctions)
-	return numberPlusScalarSquared
+# def square(numberToSquare, numberOfFunctions):
+# 	# print('in square, time = ' + str(time.monotonic()))
+# 	numberPlusScalarSquared = (numberToSquare + numberOfFunctions) * (numberToSquare + numberOfFunctions)
+# 	print(numberPlusScalarSquared)
+# 	return numberPlusScalarSquared
+
+def double(numberToDouble, numberOfJobs, numberOfFunctions):
+	# print('numberToDouble = ' + str(numberToDouble) + ' numberOfJobs = ' + str(numberOfJobs) + ' numberOfFunctions = ' + str(numberOfFunctions))
+	return 2 * numberToDouble
+
 def main():
 	plt.figure(figsize=(10, 6))
 	plt.xlabel('Number of Jobs')
@@ -30,14 +36,19 @@ def main():
 	# print('START: ' + str(time.monotonic()))
 	print("starting now")
 	for numberOfJobs in range(1, 11):
-		for numberOfFunctionCalls in range(1000, 50001):
+		for numberOfFunctionCalls in range(0, 50001):
+			#call
+
+
+
 			startTime = time.monotonic()
 
 			# print('starting: number of jobs: ' + str(numberOfJobs) + ' functionCalls = ' + str(numberOfFunctionCalls))
 
 
 
-			results = joblib.Parallel(n_jobs = numberOfJobs)(joblib.delayed(square)(i, numberOfFunctionCalls) for i in range(numberOfFunctionCalls))
+			results = joblib.Parallel(n_jobs = numberOfJobs)(joblib.delayed(double)(i, numberOfJobs, numberOfFunctionCalls) for i in range(10))
+			# results = joblib.Parallel(n_jobs = numberOfJobs)(joblib.delayed(double)(i, numberOfFunctionCalls) for i in range(numberOfFunctionCalls))
 		
 			endTime = time.monotonic()
 			# print('final time:' + str(endTime - startTime))
@@ -45,18 +56,44 @@ def main():
 			elapsedTimeList.append(endTime - startTime)
 			functionCalls.append(numberOfFunctionCalls)
 		
+			if(numberOfFunctionCalls == 5000):
+				print('numberOfJobs = ' + str(numberOfJobs) + ' numberOfFunctionCalls = ' + str(numberOfFunctionCalls))
+
+			elif(numberOfFunctionCalls == 10000):
+				print('numberOfJobs = ' + str(numberOfJobs) + ' numberOfFunctionCalls = ' + str(numberOfFunctionCalls))
+
+			elif(numberOfFunctionCalls == 15000):
+				print('numberOfJobs = ' + str(numberOfJobs) + ' numberOfFunctionCalls = ' + str(numberOfFunctionCalls))
+
+			elif(numberOfFunctionCalls == 20000):
+				print('numberOfJobs = ' + str(numberOfJobs) + ' numberOfFunctionCalls = ' + str(numberOfFunctionCalls))
+
+			elif(numberOfFunctionCalls == 25000):
+				print('numberOfJobs = ' + str(numberOfJobs) + ' numberOfFunctionCalls = ' + str(numberOfFunctionCalls))
+
+			elif(numberOfFunctionCalls == 30000):
+				print('numberOfJobs = ' + str(numberOfJobs) + ' numberOfFunctionCalls = ' + str(numberOfFunctionCalls))
+
+			elif(numberOfFunctionCalls == 35000):
+				print('numberOfJobs = ' + str(numberOfJobs) + ' numberOfFunctionCalls = ' + str(numberOfFunctionCalls))
+
+			elif(numberOfFunctionCalls == 40000):
+				print('numberOfJobs = ' + str(numberOfJobs) + ' numberOfFunctionCalls = ' + str(numberOfFunctionCalls))
+
+			elif(numberOfFunctionCalls == 45000):
+				print('numberOfJobs = ' + str(numberOfJobs) + ' numberOfFunctionCalls = ' + str(numberOfFunctionCalls))
+
+			elif(numberOfFunctionCalls == 50000):
+				print('numberOfJobs = ' + str(numberOfJobs) + ' numberOfFunctionCalls = ' + str(numberOfFunctionCalls))
+		print('out for 2nd nested for loop: numberOfJobs = ' + str(numberOfJobs) + ' numberOfFunctionCalls')
 		elapsedTimeList.append(endTime - startTime)
 		functionCalls.append(numberOfFunctionCalls)
-
 		plt.plot(functionCalls, elapsedTimeList, label=str(numberOfJobs))
 		plt.scatter(functionCalls, elapsedTimeList)
-
 		elapsedTimeList = []
 		functionCalls = []
-
 	plt.legend(loc='upper left', title='Processes',framealpha=0.8)
 	plt.title("Python joblib")
 	plt.show()
-
 if __name__ == '__main__':
     main()
